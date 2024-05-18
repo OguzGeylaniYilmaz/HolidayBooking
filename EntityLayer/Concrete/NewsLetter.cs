f2ab73e835f122930867edcc36e972ecffc0b052
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2024-05-18 22:09:55 +0200

    entities created

---
+++ EntityLayer/Concrete/NewsLetter.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityLayer.Concrete
+{
+    public class NewsLetter
+    {
+        public int NewsLetterID { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Client { get; set; }
+        public string Comment { get; set; }
+        public string ClientImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
